08e53f862a077f5c2f5fcb1f7335661a75343c52
Victor Epifânio <vepifanio1@gmail.com>
2021-06-03 09:04:11 -0300

    Repositories Fixed to receive rental id, end_date and total

--- src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
+++ src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
@@ -26,11 +26,17 @@ class RentalsRepository implements IRentalsRepository {
     car_id,
     user_id,
     expected_return_date,
+    id,
+    end_date,
+    total,
   }: ICreateRentalDTO): Promise<Rental> {
     const rental = this.repository.create({
       car_id,
       expected_return_date,
       user_id,
+      id,
+      end_date,
+      total,
     });
 
     await this.repository.save(rental);
--- src/modules/rentals/repositories/in-memory/RentalsRepositoryInMemory.ts
+++ src/modules/rentals/repositories/in-memory/RentalsRepositoryInMemory.ts
@@ -10,6 +10,9 @@ class RentalsRepositoryInMemory implements IRentalsRepository {
     car_id,
     user_id,
     expected_return_date,
+    id,
+    end_date,
+    total,
   }: ICreateRentalDTO): Promise<Rental> {
     const rental = new Rental();
 
@@ -18,6 +21,9 @@ class RentalsRepositoryInMemory implements IRentalsRepository {
       user_id,
       expected_return_date,
       start_date: new Date(),
+      id,
+      end_date,
+      total,
     });
 
     this.rentals.push(rental);
